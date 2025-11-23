fda1cfcf562dd9aa0f41f76586b8fde172d18b7d
Richiyo07 <139646928+Richiyo07@users.noreply.github.com>
2025-11-23 14:38:55 -0500

    Rename Sonar instance resources to richSonarInstance

--- sonar.tf
+++ sonar.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "mySonarInstance" {
+resource "aws_instance" "richSonarInstance" {
       ami           = "ami-0f5fcdfbd140e4ab7"
       key_name = var.key_name
       instance_type = "t2.micro"
@@ -40,7 +40,7 @@ resource "aws_instance" "mySonarInstance" {
     }
 
 # Create Elastic IP address for Sonar instance
-resource "aws_eip" "mySonarInstance" {
+resource "aws_eip" "richSonarInstance" {
   vpc      = true
   instance = aws_instance.mySonarInstance.id
 tags= {
